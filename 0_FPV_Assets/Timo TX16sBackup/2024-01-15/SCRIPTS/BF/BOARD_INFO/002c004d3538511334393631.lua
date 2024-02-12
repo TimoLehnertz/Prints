b81cf27e6219c35be6e5fb2ad4554ea31301b0b1
TimoLehnertz <57093983+TimoLehnertz@users.noreply.github.com>
2024-02-12 18:34:11 +0100

    assets

---
+++ 0_FPV_Assets/Timo TX16sBackup/2024-01-15/SCRIPTS/BF/BOARD_INFO/002c004d3538511334393631.lua
@@ -0,0 +1,16 @@
+return {
+    boardIdentifier = "S7X2",
+    hardwareRevision = 0,
+    boardType = 2,
+    targetCapabilities = 55,
+    targetName = "STM32F7X2",
+    boardName = "MAMBAF722_I2C",
+    manufacturerId = "DIAT",
+    signature = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+    mcuTypeId = 2,
+    configurationState = 64,
+    gyroSampleRateHz = 8000,
+    configurationProblems = 0,
+    spiRegisteredDeviceCount = 1,
+    i2cRegisteredDeviceCount = 0,
+}
